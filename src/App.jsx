2f38390b7cb91b89ae46f8a1ab7d1a8fef9d37bd
luiz henrique da veiga dalagnol <luizhenriqueveigadalagnol@gmail.com>
2025-04-25 14:54:15 -0300

    função error na pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState('');
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleSearch = async () => {
+    if (!username) return;
+    setError('');
+    setUserData(null);
+    setLoading(true);
+    try{
+      const response = await
+        fetch(`https://api.github.com/users/${username}`);
+      if (!response.ok) throw new Error();
+      const data = await response.json();
+      setUserData(data);
+    } catch {
+      setError('Nenhum perfil foi encontrado com ese nome de usuário. Tente novamente');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  
+
 }
-
 export default App
